0d55eeb5a874262c7667ea176212d220314188a4
ridenroam <mail2ravibhat@gmail.com>
2017-09-09 12:32:45 +0530

    mod4'

---
+++ site/module4-solution/harder/script.js
@@ -0,0 +1,11 @@
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (i=0; i< names.length; i++) {
+	
+	if (names[i].charAt(0).toLowerCase() == 'j') {
+		byeSpeaker.speak(names[i]);
+  } else {
+    helloSpeaker.speak(names[i]);
+  }
+}
